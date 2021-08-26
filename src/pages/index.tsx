8b658e58bfb53343bccda8cd8eca2261c4599ac7
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-08-26 18:04:08 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>World Trip</h1>
+  )
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      '600': '#FFBA08'
+    },
+    gray: {
+      '600': '#47585B',
+      '300': '#999999',
+      '200': '#DADADA',
+      '100': '#F5F8FA',
+      '50': '#FFFFFF'
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.100',
+        color: 'gray.600'
+      }
+    }
+  }
+})
